9a0f836b6b60de7b9447cfb16b51098fbae0816e
maxch0240 <maxch.0240@gmail.com>
2021-08-01 03:28:46 +0300

    scv to db(cleared comments)

--- src/Main.java
+++ src/Main.java
@@ -22,35 +22,3 @@ public class Main {
 
     }
 }
-
-
-//        String url = "jdbc:mysql://localhost:3306/strings";
-//        String username = "root";
-//        String password = "1234";
-//        Class.forName("com.mysql.jdbc.Driver");
-//
-//        File file = new File("D:\\JAVA\\task1.1\\files\\2.txt");
-//        int allFields = count("D:\\JAVA\\task1.1\\files\\2.txt");
-//        System.out.println("file: " + file.getName());
-//
-//
-//        try (Connection conn = DriverManager.getConnection(url, username, password)) {
-//        BufferedReader reader = new BufferedReader(new FileReader(file));
-//        int counter = 0;
-//
-//        String line;
-//        while ((line = reader.readLine()) != null) {
-//        String[] arrSplit = line.split("\\|\\|");
-//        PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO strings VALUES (?, ?, ?, ?, ?);");
-//        preparedStatement.setString(1, arrSplit[0]);
-//        preparedStatement.setString(2, arrSplit[1]);
-//        preparedStatement.setString(3, arrSplit[2]);
-//        preparedStatement.setString(4, String.valueOf(Integer.parseInt(arrSplit[3])));
-//        preparedStatement.setString(5, String.valueOf(Double.parseDouble(arrSplit[4])));
-//        preparedStatement.executeUpdate();
-//        counter++;
-//        System.out.println("fields left: " + (allFields - counter));
-//
-//        }
-//        reader.close();
-//
